84293baa5a413f6c25254faf3272f8be5a7f5cf4
Ougodouloupte <mrguigui0910@gmail.com>
2021-09-15 12:04:52 +0200

    Map + joueur + shader (projet)
    début de map et visuel

---
+++ HauntedGarden/Assets/Script/camera.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+    public Vector2 mousePosition = new Vector2();
+    public Vector3 rotationS;
+    float timer = 0;
+    public float speed = 6;
+    public float originalSpeed;
+    public Quaternion rotationFinal;
+    public bool inCinematic;
+    public bool ded;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        originalSpeed = speed;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (inCinematic || ded)
+        {
+            return;
+        }
+        // partie caméra
+        float h = Input.GetAxis("Mouse X");
+        float v = Input.GetAxis("Mouse Y");
+        mousePosition = new Vector2(h, v);
+
+        Vector3 currentRotation = transform.rotation.eulerAngles;
+        rotationS = currentRotation;
+        if ((currentRotation.x - v >= 90 && currentRotation.x - v <= 306))
+        {
+            return;
+        }
+        rotationFinal = Quaternion.Euler(currentRotation.x - v, currentRotation.y + h, 0);
+        transform.rotation = rotationFinal;
+
+        // partie tremblement 
+        if (Input.GetKey(KeyCode.Z) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.D))
+        {
+           
+        }
+    }
+
+    public Quaternion getRotation()
+    {
+        return rotationFinal;
+    }
+}
---
+++ HauntedGarden/Assets/Script/test.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    public float speed;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovementInputs();
+    }
+
+
+    void MovementInputs()
+    {
+        Vector3 dir = new Vector3(0, 0, 0);
+        if (Input.GetKey(KeyCode.Z))
+        {
+            // MoveDirection(transform.forward);
+            dir += transform.forward;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            //MoveDirection(-transform.forward);
+            dir += -transform.forward;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            //MoveDirection(transform.right);
+            dir += transform.right;
+        }
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            //MoveDirection(-transform.right);
+            dir += -transform.right;
+        }
+
+        MoveDirection(dir);
+    }
+
+    void MoveDirection(Vector3 direction)
+    {
+        Vector3 forward = direction;
+        forward.y = 0.0f;
+        forward.Normalize();
+        forward *= speed;
+        //physicalForm.transform.Translate((new Vector3(forward.x, 0, forward.z) * Time.deltaTime));
+        rb.velocity = forward;
+    }
+}
